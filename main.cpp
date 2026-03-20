b86264003164ae83a19e75a08ffab539933f6540
calyptr4 <alessandrabaires@gmail.com>
2026-03-19 23:47:31 -0700

    a45

--- main.cpp
+++ main.cpp
@@ -11,7 +11,19 @@ int main()
     cout << "Enter three integer values: ";
     cin >> num1 >> num2 >> num3;
 
-    // TODO: Find min, max, and med among num1, num2, num3
+    min = num1;
+    min = (num2 < min) ? num2 : min;
+    min = (num3 < min) ? num3 : min;
+    
+    max = num1;
+    max = (num2 > max) ? num2 : max;
+    max = (num3 > max) ? num3 : max;
+
+    if (num1 != min && num1 != max){
+        med = num1;
+    } else if (num2 != min && num2 != max){
+        med= num2;
+    } else med = num3;
 
     cout << min << " " << med << " " << max << endl;
 }
